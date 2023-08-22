a673bc62e8e5560029bd09ea980209bf63bf934f
xyyy <yyyh1489@gmail.com>
2023-08-22 11:19:34 +0800

    update

--- src/app/shader/shanghai/shanghai.component.ts
+++ src/app/shader/shanghai/shanghai.component.ts
@@ -21,6 +21,8 @@ import {
   ShaderMaterial,
   Box3,
   Vector3,
+  EdgesGeometry,
+  LineSegments,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
@@ -116,13 +118,14 @@ export class ShanghaiComponent implements OnInit {
     this.scene.add(light);
     const loader = new FBXLoader()
     this.camera.far = 10000;
-    this.camera.position.set(1500, 800, 2000);
+    this.camera.position.set(800, 400, 800);
     this.camera.updateProjectionMatrix();
     loader.load('assets/model/shanghai.FBX', (scene) => { //
       this.scene.add(scene);
       scene.traverse(child => {
         if (child.name === meshName) {
-          this.setMaterial(child)
+          this.setMaterial(child);
+          this.aroundLine(child);
         }
       })
     })
@@ -157,7 +160,6 @@ export class ShanghaiComponent implements OnInit {
       max.z - min.z
     );
 
-    console.log(size)
     mesh.material = new ShaderMaterial({
       uniforms: {
         uOpacity: {
@@ -177,10 +179,153 @@ export class ShanghaiComponent implements OnInit {
 
   }
 
+  aroundLine(mesh: any) {
+    const geometry = new EdgesGeometry(mesh.geometry);
+    const worldPosition = new Vector3();
+    mesh.getWorldPosition(worldPosition);
+
+    const { max, min } = mesh.geometry.boundingBox;
+
+    const size = new Vector3(
+      max.x - min.x,
+      max.y - min.y,
+      max.z - min.z
+    )
+
+    const material = this.lineMaterial({ max, min, size });
+
+    const line = new LineSegments(geometry, material);
+
+    line.name = "aroundLine";
+    line.scale.copy(mesh.scale);
+    line.rotation.copy(mesh.rotation);
+    line.position.copy(worldPosition);
+
+    this.scene.add(line);
+
+  }
+  lineShaderMaterial!: ShaderMaterial;
+
+  lineMaterial({
+    max,
+    min,
+    size
+  }: any) {
+    if (this.lineShaderMaterial) return this.lineShaderMaterial;
+
+    this.lineShaderMaterial = new ShaderMaterial({
+      transparent: true,
+      uniforms: {
+        uColor: {
+          value: new Color("#ffffff")
+        },
+        uActive: {
+          value: new Color("#000000")
+        },
+        uTime: {
+          value: 0.1
+        },
+        uOpacity: {
+          value: 0.6
+        },
+        uMax: {
+          value: max,
+        },
+        uMin: {
+          value: min,
+        },
+        uRange: {
+          value: 200
+        },
+        uSpeed: {
+          value: 0.2
+        },
+        uStartTime: {
+          value: 0.1
+        }
+      },
+      // 顶点着色器的GLSL代码。
+      vertexShader: `
+                    #define PI 3.14159265359
+
+                    uniform mediump float uStartTime;
+                    uniform mediump float uTime;
+                    uniform mediump float uRange;
+                    uniform mediump float uSpeed;
+                
+                    uniform vec3 uColor;
+                    uniform vec3 uActive;
+                    uniform vec3 uMin;
+                    uniform vec3 uMax;
+                
+                    varying vec3 vColor;
+                
+                    float lerp(float x, float y, float t) {
+                        return (1.0 - t) * x + t * y;
+                    }
+                    void main() { 
+                        if (uStartTime >= 0.99) {
+                            float iTime = mod(uTime * uSpeed - uStartTime, 1.0);
+                            float rangeY = lerp(uMin.y, uMax.y, iTime);
+                            if (rangeY < position.y && rangeY > position.y - uRange) {
+                                float index = 1.0 - sin((position.y - rangeY) / uRange * PI);
+                                float r = lerp(uActive.r, uColor.r, index);
+                                float g = lerp(uActive.g, uColor.g, index);
+                                float b = lerp(uActive.b, uColor.b, index);
+                
+                                vColor = vec3(r, g, b);
+                                // vColor = vec3(0.0, 0.0, 0.0);
+                            } else {
+                                vColor = uColor;
+                            }
+                        }
+                        vec3 vPosition = vec3(position.x, position.y, position.z * uStartTime);
+                        gl_Position = projectionMatrix * modelViewMatrix * vec4(vPosition, 1.0);
+                    } 
+      `,
+      fragmentShader: `
+                    precision mediump float;
+
+                    float distanceTo(vec2 src, vec2 dst) {
+                        float dx = src.x - dst.x;
+                        float dy = src.y - dst.y;
+                        float dv = dx * dx + dy * dy;
+                        return sqrt(dv);
+                    } 
+                    
+                    float lerp(float x, float y, float t) {
+                        return (1.0 - t) * x + t * y;
+                    }
+                    
+                    #define PI 3.14159265359
+                    #define PI2 6.28318530718
+                    uniform float uTime;
+                    uniform float uOpacity;
+                    uniform float uStartTime;
+
+                    varying vec3 vColor; 
+
+                    void main() {
+
+                        gl_FragColor = vec4(vColor, uOpacity * uStartTime);
+                    }
+      `
+    });
+
+    return this.lineShaderMaterial;
+  }
+
   animation() {
+    // console.log(this.scene);
     this.scene.traverse(function (object: any) {
       if (object.name === meshName) {
-        object.material.uniforms.uTime.value += 0.02
+        object.material.uniforms.uTime.value += 0.02;
+      }
+      if (object.name === "aroundLine") {
+
+        if (object.material.uniforms.uStartTime.value < 1.0) {
+          object.material.uniforms.uStartTime.value += 0.02;
+        }
 
       }
     });
