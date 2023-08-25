d14a078d6f8e4aaf83a27340a172e5e2fff50c5c
xyyy <yyyh1489@gmail.com>
2023-08-25 17:10:37 +0800

    update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-index',
@@ -10,6 +11,28 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    anime({
+      targets: '.twitter',
+      translateX: 100,
+      direction: 'alternate',
+      easing: 'easeInOutQuad'
+    });
+
+    anime({
+      targets: '.card-container .card',
+      scale: [
+        { value: .01, easing: 'easeOutSine', duration: 600 },
+        { value: 1, easing: 'easeOutSine', duration: 600 }
+      ],
+      delay: anime.stagger(200, { from: 'first' })
+    });
+
+    // anime({
+    //   targets: '',
+    //   translateX: -270,
+    //   rotate: anime.stagger([-360, 360]),
+    //   easing: 'easeInOutQuad'
+    // });
   }
 
 }
